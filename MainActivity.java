304af60f73c728b47b127788841d56d623b9f9a6
Abhishek Kadam <akadam07.ak@gmail.com>
2018-11-29 00:22:03 +0530

    second commit.

--- MainActivity.java
+++ MainActivity.java
@@ -1,60 +1,82 @@
 package com.stupidgames.xandos;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    int[][] winningPositions = { {0,1,2},{3,4,5},{6,7,8},{0,4,8},
+            {2,4,6},{0,3,6},{1,4,7},{2,5,8}};
+
+
+    int[] placed = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+
+    int activePlayer = 0; // 0 = android and 1= iphone
+
+    public void boxClicked(View view) {
+
+        ImageView boxClicked = (ImageView) view;
+
+        int tappedBox = Integer.parseInt( boxClicked.getTag().toString() );
+
+
+        if (placed[tappedBox] == 2) {
+
+            placed[tappedBox] = activePlayer;
+
+
+            boxClicked.setTranslationY( -1000f );
+
+            if (activePlayer == 0) {
+                boxClicked.setImageResource( R.drawable.android_o );
+                activePlayer = 1;
+            } else {
+                boxClicked.setImageResource( R.drawable.iphone_x );
+                activePlayer = 0;
+            }
+
+            boxClicked.animate()
+                    .translationYBy( 1000f )
+                    .rotationX( 720 )
+                    .setDuration( 500 );
+
+
+            for (int[] winningPosition:winningPositions){
+                if (placed[winningPosition[0]]!= 2
+                        && placed[winningPosition[0]]== placed[winningPosition[1]]
+                        && placed[winningPosition[1]]== placed[winningPosition[2]]){
+
+                    //someone has won here
+                    Toast.makeText( this,"Someone has won",Toast.LENGTH_SHORT ).show();
+
+                    LinearLayout linearLayout = findViewById( R.id.playAgain );
+
+                    linearLayout.setVisibility( View.VISIBLE );
+
+
+
+                }
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_layout );
 
-        ImageView topLeft= findViewById( R.id.ivTopLeft );
-        ImageView topMid= findViewById( R.id.ivTopMid);
-        ImageView topRight= findViewById( R.id.ivTopRight );
-        ImageView midLeft= findViewById( R.id.ivMidLeft);
-        ImageView midMid= findViewById( R.id.ivMidMid);
-        ImageView midRight= findViewById( R.id.ivMidRight);
-        ImageView bottomLeft= findViewById( R.id.ivBottomLeft);
-        ImageView bottomMid= findViewById( R.id.ivBottomMid);
-        ImageView bottomRight= findViewById( R.id.ivBottomRight );
-
-
-        View.OnClickListener listener = new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                ImageView counter = (ImageView) v;
-
-                counter.setTranslationY( -1000f );
-                counter.setImageResource( R.drawable.iphone_x );
-                counter.animate()
-                        .translationXBy( 1000f )
-                        .rotation( 720 )
-                        .setDuration( 3000 );
-
-            }
-        };
-        topLeft.setOnClickListener( listener );
-        topMid.setOnClickListener( listener );
-        topRight.setOnClickListener( listener );
-        midLeft.setOnClickListener( listener );
-        midMid.setOnClickListener( listener );
-        midRight.setOnClickListener( listener );
-        bottomLeft.setOnClickListener( listener );
-        bottomMid.setOnClickListener( listener );
-        bottomRight.setOnClickListener( listener );
-
-
-
-
-
-
-
 
     }
 }
+
+
+
+
+
+
